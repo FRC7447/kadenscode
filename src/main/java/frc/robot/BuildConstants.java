b46bbc0f5b814838b8a398993df082afb943c20b
Kaden Wong <kdw2x@icloud.com>
2025-06-20 11:40:12 -0700

    Feat: Update to use CANCoders
    Signed-off-by: Kaden Wong <kdw2x@icloud.com>

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "MyRoninBasee";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "20d49d5e581d17eca639ce58b80530369f4dd2e0";
-  public static final String GIT_DATE = "2025-05-06 23:25:10 EDT";
+  public static final int GIT_REVISION = 4;
+  public static final String GIT_SHA = "e553d5456abdd3ab9deba428b17a65ae298ace07";
+  public static final String GIT_DATE = "2025-06-20 14:25:11 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-06-17 14:25:10 EDT";
-  public static final long BUILD_UNIX_TIME = 1750184710535L;
+  public static final String BUILD_DATE = "2025-06-20 14:36:19 EDT";
+  public static final long BUILD_UNIX_TIME = 1750444579251L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
