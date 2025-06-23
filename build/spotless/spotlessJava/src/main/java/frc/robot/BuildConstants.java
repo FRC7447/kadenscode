fd3242ed385ea76b248f0a58029fe82345101c62
Kaden Wong <kdw2x@icloud.com>
2025-06-23 11:29:17 -0700

    Feat: Vision template
    Signed-off-by: Kaden Wong <kdw2x@icloud.com>

--- build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
+++ build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "MyRoninBasee";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 5;
-  public static final String GIT_SHA = "b46bbc0f5b814838b8a398993df082afb943c20b";
-  public static final String GIT_DATE = "2025-06-20 14:40:12 EDT";
+  public static final int GIT_REVISION = 7;
+  public static final String GIT_SHA = "f444496c3d8f1774c6747f6b730faaeca372c17b";
+  public static final String GIT_DATE = "2025-06-20 15:25:09 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-06-20 14:46:42 EDT";
-  public static final long BUILD_UNIX_TIME = 1750445202272L;
+  public static final String BUILD_DATE = "2025-06-23 14:05:42 EDT";
+  public static final long BUILD_UNIX_TIME = 1750701942233L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
